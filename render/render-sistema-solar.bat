815138169d036bce26ea4a6c2e80c33074b29683
Vinicius Josina <Viniciusburgie@gmail.com>
2025-05-26 17:45:21 -0300

    chore: reorganiza arquivos

--- render/render-sistema-solar.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-"C:\Program Files\POV-Ray\v3.7\bin\pvengine.exe" ^
-+I"C:\Repositorio Git\projeto-povray\cenas\sistema-solar.pov" ^
-+O"C:\Users\Burgi\Documents\POV-Ray\v3.7\bin\sistema-solar.png" ^
-+FN +W1920 +H800 ^
-/RENDER /EXIT
-pause
